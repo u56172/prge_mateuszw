46a40decc9ac3c062b97bdb0b0a46c605abe2a9d
Mateusz <u56172@student.wat.edu.pl>
2025-12-08 19:58:59 +0100

    feat: input of home component and style for it

--- config/node/frontend/src/App.js
+++ config/node/frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="app">
         <Home/>
+
     </div>
   );
 }
--- config/node/frontend/src/pages/Home.js
+++ config/node/frontend/src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Typography} from "@mui/material";
+import {Button} from "@mui/material";
+
 
 function Home(props) {
     return (
-        <div>strona startowa react</div>
+        <div className='home'>
+            <h1 className='home__tittle'>GEOPORTAL</h1>
+            <Typography className='home__subtitle'>
+                Geoportal tematyczny poświęcony danym przestrzennym
+            </Typography>
+
+            <Button className='home__button' variant='contained' size='large'>START</Button>
+
+        </div>
     );
 }
 
