2c40ad0376b051201607c04f196d39bcd3b3e00d
Mateusz <u56172@student.wat.edu.pl>
2025-12-20 00:41:49 +0100

    fix: try to fix fastapi v4

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -5,10 +5,12 @@ from app.settings import db_name, db_user, db_password
 
 router_insert = APIRouter()
 
+
 def connect_to_db(db_name: str, db_user: str, db_password: str):
     return create_engine(
         f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}"
     )
+
 
 @router_insert.get("/insert_user")
 async def insert_user():
@@ -22,20 +24,18 @@ async def insert_user():
         }
 
         sql_query = text("""
-                            INSERT INTO public.users (name, posts, location)
-                            VALUES (:name, :posts, :location);
+                         insert into users (name, posts, location)
+                         values (:name, :posts, :location); \
                          """)
-
 
         with db_connection.connect() as conn:
             result = conn.execute(sql_query, params)
             conn.commit()
             print(result)
 
+
     except Exception as e:
         print(e)
         raise e
 
     return {"statu": 1}
-
-
